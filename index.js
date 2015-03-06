b50ccf8a3b7e90b94a3fb2996e397f2526871f76
James Burnett <jtburnett@tribune.com>
2015-03-06 14:50:23 -0500

    made lazy-input even lazier with a procrastination timer

--- index.js
+++ index.js
@@ -3,11 +3,31 @@ var React = require('react')
 
 var LazyInput = React.createClass({
   displayName: "LazyInput",
+  propTypes: {
+    type: React.PropTypes.string,               // ['text'] type of input/textarea
+    lazyLevel: React.PropTypes.number           // [1000]   number of ms to wait before responding to changes in prop.value
+    // note: passes through everything but lazyLevel
+  },
+  getDefaultProps: function() {
+    return {
+      type: 'text',
+      lazyLevel: 1000
+    }
+  },
   getInitialState: function() {
     return { value: this.props.value };
   },
   componentWillReceiveProps: function(nextProps) {
-    this.setState({ value: nextProps.value });
+    this.setState({ requestedValue: nextProps.value });
+    this.doItLater();
+  },
+  doItLater: function() {
+    if(this.procrastinating) { clearTimeout(this.procrastinating); }
+    this.procrastinating = setTimeout(this.ohAlrightAlready, this.props.lazyLevel);
+  },
+  ohAlrightAlready: function() {
+    this.procrastinating = undefined;
+    this.setState({ value: this.state.requestedValue });
   },
   onChange: function(event) {
     this.setState({ value: event.target.value });
@@ -17,6 +37,7 @@ var LazyInput = React.createClass({
     // for the most part, we are just going to pass through whatever comes in
     var props = clone(this.props);
     props.value = this.state.value;
+    delete props.lazyLevel;
     if(props.onChange) { props.onChange = this.onChange; }
     return props;
   },
